49cca2546c2c2e47c62dc8fce63d4ddc0fa83623
Arzivre <sonyfauzi@outlook.co.id>
2022-10-23 23:16:32 +0700

    add Date

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 30 column grid
+        28: "repeat(28, minmax(0, 1fr))",
+        29: "repeat(29, minmax(0, 1fr))",
+        30: "repeat(30, minmax(0, 1fr))",
+        31: "repeat(31, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
