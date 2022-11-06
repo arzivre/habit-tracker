e212bfcd1630cdeb1d3d5949f9b2f3a9ea130451
Arzivre <sonyfauzi@outlook.co.id>
2022-11-06 20:08:05 +0700

    Update Habit function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,17 +119,17 @@ const Home = () => {
       </main>
       <article className=" ">
         <ul>
-          {habits.data?.map(({ title, id }) => (
-            <li key={id} className="mb-2 w-full bg-[#e3f6f5] shadow">
+          {habits.data?.map((habit) => (
+            <li key={habit.id} className="mb-2 w-full bg-[#e3f6f5] shadow">
               <div className="grid grid-cols-[300px_auto]">
-                <HabitTitle id={id}>{title}</HabitTitle>
+                <HabitTitle {...habit} />
 
                 <section>
                   <HabitRecords
-                    key={id}
-                    demoHabitId={id}
-                    month={currentMonth}
                     days={days}
+                    key={habit.id}
+                    month={currentMonth}
+                    demoHabitId={habit.id}
                   />
                 </section>
               </div>
@@ -143,12 +143,15 @@ const Home = () => {
 };
 
 interface HabitTitleProps {
-  children: string;
   id: string;
+  title: string;
+  filterId: string;
 }
-const HabitTitle = ({ children, id }: HabitTitleProps) => {
-  const utils = trpc.useContext();
+const HabitTitle = ({ title, id, filterId }: HabitTitleProps) => {
+  const [showForm, setShowForm] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
 
+  const utils = trpc.useContext();
   const deleteHabit = trpc.demo.deleteHabit.useMutation({
     async onMutate(deleteHabit) {
       await utils.demo.getHabits.cancel();
@@ -169,35 +172,150 @@ const HabitTitle = ({ children, id }: HabitTitleProps) => {
     },
   });
 
+  const updateHabit = trpc.demo.updateHabit.useMutation({
+    async onMutate(updateHabit) {
+      await utils.demo.getHabits.cancel();
+
+      const prevHabits = utils.demo.getHabits.getData();
+
+      utils.demo.getHabits.setData(
+        prevHabits?.filter((habit) => {
+          if (habit.id === updateHabit.id) {
+            return updateHabit;
+          }
+          return habit;
+        })
+      );
+
+      return { prevHabits };
+    },
+    onError(error, variables, context) {
+      utils.demo.getHabits.setData(context?.prevHabits);
+    },
+    onSettled() {
+      utils.demo.getHabits.invalidate();
+    },
+  });
+
   function handleDelete() {
     deleteHabit.mutate({ id });
   }
 
-  return (
-    <div className="flex justify-between pl-2 font-semibold">
-      <p className="text-[#2d334a]">{children}</p>
-      <span className="pr-1 text-red-500">
-        <button onClick={handleDelete} disabled={deleteHabit.isLoading}>
-          {deleteHabit.isLoading ? (
-            "Loading"
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-4 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-              />
-            </svg>
-          )}
+  function handleSubmit() {
+    if (newTitle === "") {
+      return setShowForm(false);
+    }
+    updateHabit.mutate({ id, title: newTitle, filterId });
+    setNewTitle("");
+    setShowForm(false);
+  }
+
+  if (showForm) {
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="grid grid-cols-[1fr_auto_auto] bg-white"
+      >
+        <input
+          type="text"
+          onChange={(e) => setNewTitle(e.target.value)}
+          value={newTitle}
+          placeholder="Habit Title"
+          className="pl-2 focus:rounded-none"
+        />
+
+        <button
+          type="submit"
+          className="ml-2 w-full bg-green-400 font-bold uppercase
+                 text-green-900 hover:bg-green-600 hover:text-green-100"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-4 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
         </button>
-      </span>
+        <button
+          onClick={() => setShowForm(false)}
+          className="bg-red-200 text-red-900"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-4 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </form>
+    );
+  }
+
+  return (
+    <div
+      className="grid grid-cols-[1fr_auto_auto] justify-between pl-2 
+    font-semibold hover:bg-[#bae8e8]"
+    >
+      <p className="text-[#272343]">{title}</p>
+      <button
+        onClick={() => setShowForm(true)}
+        className="text-[#2d334a] hover:bg-green-100 hover:text-green-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-4 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </button>
+      <button
+        onClick={handleDelete}
+        disabled={deleteHabit.isLoading}
+        className="text-[#2d334a] hover:bg-red-100 hover:text-red-600"
+      >
+        {deleteHabit.isLoading ? (
+          "Loading"
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-4 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        )}
+      </button>
     </div>
   );
 };
