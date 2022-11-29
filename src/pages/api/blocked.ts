7bed6770ede64a398f9a10e0ba98c26b7ef7042f
Arzivre <sonyfauzi@outlook.co.id>
2022-11-29 11:09:06 +0700

    delete example api then add block api

---
+++ src/pages/api/blocked.ts
@@ -0,0 +1,7 @@
+// src/pages/api/blocked.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(429);
+  return res.end();
+}
